75bcd13bad134bdeeaef395949cdcc4bff8c2617
Posh <tautvydasposkus@mail.com>
2017-04-12 14:07:32 +0300

    Pilnaspirmaslaboras+saugojimas liko padaryt validacija ant naujo uzsakymo

--- app/src/main/java/com/example/posh/preggietest/MainActivity.java
+++ app/src/main/java/com/example/posh/preggietest/MainActivity.java
@@ -5,8 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mUsernameView;
+    private EditText mPasswordView;
+    private String username;
+    private String password;
 
 
 
@@ -15,14 +24,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mUsernameView = (EditText) findViewById(R.id.username);
+        mPasswordView = (EditText) findViewById(R.id.password);
+
+        mUsernameView.setError(null);
+        mPasswordView.setError(null);
+
         Button prisijungti = (Button) findViewById(R.id.prijungti);
         prisijungti.setOnClickListener(new View.OnClickListener() {
 
-            public void onClick(View v) {
-                // TODO Auto-generated method stub
-                Intent myIntent = new Intent(MainActivity.this, MeniuActivity.class);
-                startActivity(myIntent);
-            }
+
+
+                @Override
+                    public void onClick(View arg0) {
+                    username = mUsernameView.getText().toString();
+                    password = mPasswordView.getText().toString();
+
+                    boolean cancel = false;
+                    View focusView = null;
+                    if (!isValid(username)){
+                        mUsernameView.setError("Neįvestas prisijungimo vardas");
+                        focusView = mUsernameView;
+                        cancel = true;
+                    }
+                    if (!isValid(password)){
+                        mPasswordView.setError("Neįvestas slaptažodis");
+                        focusView = mPasswordView;
+                        cancel = true;
+                    }
+                    if (cancel){
+                        focusView.requestFocus();
+                    }
+                    else {
+                        Intent myIntent = new Intent(MainActivity.this, MeniuActivity.class);
+                        startActivity(myIntent);
+                    }
+                }
+
+
+
         });
 
         Button registracija = (Button) findViewById(R.id.registracija);
@@ -35,5 +75,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    private boolean isValid(String credentials) {
+        final String CREDENTIALS_PATTERN = "^([0-9a-zA-z]{3,15})+$";
+        Pattern pattern = Pattern.compile(CREDENTIALS_PATTERN);
+
+        Matcher matcher = pattern.matcher(credentials);
+        return matcher.matches();
     }
 }
